2dd0b25a5af7a2e935ffe49a393199bf16c83b44
Diego Maruzy <50217535+maruzy-eng@users.noreply.github.com>
2026-07-16 15:28:33 -0300

    update design

--- src/app/lp/page.tsx
+++ src/app/lp/page.tsx
@@ -296,10 +296,10 @@ export default async function FlipHouseLandingPage() {
 
             <h1 className="cmp-hero-title">
               <span className="cmp-hero-line">
-                Aprenda os fundamentos de Flip House 
-              </span><b> </b>
+                Aprenda os fundamentos de Flip House
+              </span>{" "}
               <span className="cmp-gradient-text">
-                 e conheça ferramentas para analisar propriedades nos Estados
+                e conheça ferramentas para analisar propriedades nos Estados
                 Unidos
               </span>
             </h1>
